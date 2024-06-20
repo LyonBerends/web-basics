9d4ec4e845dd8c7b7986997413ea9713f0d47a36
Lyon4054 <lyonberends@live.nl>
2024-06-20 16:51:43 +0200

    backend

--- src/database.js
+++ src/database.js
@@ -4,26 +4,65 @@ const db = new Database('database.db', { verbose: console.log });
 exports.get_artist = {
     from_id : function(id) {
         const stmt = db.prepare("SELECT * FROM Artist WHERE artist_id = ?");
-        const artist = stmt.get(id);
-        return artist;
+        return stmt.get(id);
     },
     from_name : function(name) {
         const stmt = db.prepare("SELECT * FROM Artist WHERE name = ?");
-        const artist = stmt.get(name);
-        return artist;
+        return stmt.get(name);
     },
     all : function() {
-        const stmt = db.prepare("SELECT * FROM Artist");
-        const artists = stmt.all();
-        return artists;
+        return db.prepare("SELECT * FROM Artist").all();
     }
 };
+
+exports.delete_artist = function(id) {
+    if(db.prepare("SELECT count(*) count FROM Artist WHERE artist_id = ?").get(id)["count"] === 0) {return 404;}
+    const stmt = db.prepare("DELETE FROM Artist WHERE artist_id = ?");
+    stmt.run(id);
+    return 200;
+}
+
+exports.delete_album = function(id) {
+    if(db.prepare("SELECT count(*) count FROM Album WHERE album_id = ?").get(id)["count"] === 0) {return 404;}
+    const stmt = db.prepare("DELETE FROM Album WHERE album_id = ?");
+    stmt.run(id);
+    return 200;
+}
+
+exports.delete_song = function(id) {
+    if(db.prepare("SELECT count(*) count FROM Song WHERE song_id = ?").get(id)["count"] === 0) {return 404;}
+    const stmt = db.prepare("DELETE FROM Song WHERE song_id = ?");
+    stmt.run(id);
+    return 200;
+}
 
 exports.new_artist = function(name, icon="missing.png") {
     try {
         const stmt = db.prepare("INSERT INTO Artist (name, icon) VALUES (?, ?)");
         stmt.run(name, icon);
-        return 200;
+        return db.prepare("SELECT * FROM Artist ORDER BY artist_id DESC limit 1").get();
+    }
+    catch (e) {
+        return e;
+    }
+}
+
+exports.new_album = function(name, artist_id, icon="missing.png") {
+    try {
+        const stmt = db.prepare("INSERT INTO Album (artist_id, name, icon) VALUES (?, ?, ?)");
+        stmt.run(artist_id, name, icon);
+        return db.prepare("SELECT * FROM Album ORDER BY album_id DESC limit 1").get();
+    }
+    catch (e) {
+        return e;
+    }
+}
+
+exports.new_song = function(name, duration, album_id, lyrics = "null", icon="missing.png") {
+    try {
+        const stmt = db.prepare("INSERT INTO Song (name, duration, album_id, lyrics, icon) VALUES (?, ?, ?, ?, ?)");
+        stmt.run(name, duration, album_id, lyrics, icon);
+        return db.prepare("SELECT * FROM Song ORDER BY song_id DESC limit 1").get();
     }
     catch (e) {
         return e;
@@ -33,35 +72,35 @@ exports.new_artist = function(name, icon="missing.png") {
 exports.get_album = {
     from_id : function(id) {
         const stmt = db.prepare("SELECT * FROM Album WHERE album_id = ?");
-        const album = stmt.get(id);
-        return album;
+        return stmt.get(id);
     },
     from_name : function(name) {
         const stmt = db.prepare("SELECT * FROM Album WHERE name = ?");
-        const album = stmt.get(name);
-        return album;
+        return stmt.get(name);
     },
     from_artist_id : function(artist_id) {
         const stmt = db.prepare("SELECT * FROM Album WHERE artist_id = ?");
-        const albums = stmt.all(artist_id);
-        return albums;
+        return stmt.all(artist_id);
+    },
+    all : function() {
+        return db.prepare("SELECT * FROM Album").all();
     }
 }
 
 exports.get_song = {
     from_id : function(id) {
         const stmt = db.prepare("SELECT * FROM Song WHERE song_id = ?");
-        const song = stmt.get(id);
-        return song;
+        return stmt.get(id);
     },
     from_name : function(name) {
         const stmt = db.prepare("SELECT * FROM Song WHERE name = ?");
-        const song = stmt.get(name);
-        return song;
+        return stmt.get(name);
     },
     from_album_id : function(album_id) {
         const stmt = db.prepare("SELECT * FROM Song WHERE album_id = ?");
-        const songs = stmt.all(album_id);
-        return songs;
+        return stmt.all(album_id);
+    },
+    all : function() {
+        return db.prepare("SELECT * FROM Song").all();
     }
 }
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const port = 3000;
 
 app.use(express.static('../public'));
 app.use('/images', express.static('../images'));
+app.use(express.json());
 
 app.get('/api/artists/:query', (req, res) => {
   const artist = db.get_artist.from_id(req.params.query);
@@ -24,12 +25,45 @@ app.get('/api/songs/:query', (req, res) => {
 });
 
 app.post('/api/artists', (req, res) => {
-  if(req.headers['content-type'] == 'json' || req.headers['content-type'] == 'application/json') {
-    res.send({"hello": "world"});
+  if(req.headers['content-type'] === 'json' || req.headers['content-type'] === 'application/json') {
+    const name = req.body["name"];
+    const image = req.body["image"];
+    if(!name) { return res.sendStatus(400); }
+    res.send(db.new_artist(name, image));
   }
   else
   {
-    res.send("Hello, world!");
+    res.sendStatus(400);
+  }
+});
+
+app.post('/api/albums', (req, res) => {
+  if(req.headers['content-type'] === 'json' || req.headers['content-type'] === 'application/json') {
+    const name = req.body["name"];
+    const artist_id = req.body["artist_id"];
+    const image = req.body["image"];
+    if(!name || !artist_id) { return res.sendStatus(400); }
+    res.send(db.new_album(name, artist_id, image));
+  }
+  else
+  {
+    res.sendStatus(400);
+  }
+});
+
+app.post('/api/songs', (req, res) => {
+  if(req.headers['content-type'] === 'json' || req.headers['content-type'] === 'application/json') {
+    const name = req.body["name"];
+    const duration = req.body["duration"];
+    const album_id = req.body["album_id"];
+    const lyrics = req.body["lyrics"];
+    const image = req.body["image"];
+    if(!name || !artist_id) { return res.sendStatus(400); }
+    res.send(db.new_song(name, duration, album_id, lyrics, image));
+  }
+  else
+  {
+    res.sendStatus(400);
   }
 });
 
@@ -37,8 +71,16 @@ app.put('/api', (req, res) => {
   res.send('Got a PUT request at /user')
 });
 
-app.delete('/api', (req, res) => {
-  res.send('Got a DELETE request at /user')
+app.delete('/api/artists/:query', (req, res) => {
+  res.send(db.delete_artist(req.params.query));
+});
+
+app.delete('/api/albums/:query', (req, res) => {
+  res.send(db.delete_album(req.params.query));
+});
+
+app.delete('/api/songs/:query', (req, res) => {
+  res.send(db.delete_song(req.params.query));
 });
 
 app.listen(port, () => {
